ae8585b78a1c90ad4064b28208b183c221bdf8c2
Luis Finke <luis@Luiss-Mac-mini.home>
2018-11-10 15:11:39 -0500

    Initial Commit

---
+++ ScreamBoy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreamBoy
+//
+//  Created by Luis Finke on 11/10/18.
+//  Copyright © 2018 Luis Finke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
